584921d686fccb3741eff9d78320c449d0bc1ed5
Sohag Hasan <mailsohaghasan@gmail.com>
2022-02-17 03:13:17 +0600

    Update money.js

--- js/money.js
+++ js/money.js
@@ -2,26 +2,29 @@ document.getElementById('calculation').addEventListener('click', function () {
     // income field
     const incomeField = document.getElementById('income');
     const totalIncome = incomeField.value;
+    incomeField.value = '';
 
     // food field
     const foodField = document.getElementById('food');
     const foodData = foodField.value;
-
+    foodField.value = '';
 
     // rent field
     const rentField = document.getElementById('rent');
     const rentData = rentField.value;
+    rentField.value = '';
 
     // cloth field
     const clothField = document.getElementById('cloth');
     const clothData = clothField.value;
+    clothField.value = '';
 
     // food and cloth  and rent field sum
     const totalExpense = parseFloat(foodData) + parseFloat(rentData) + parseFloat(clothData);
     // minus total-income to total-expanse
     const totalBalance = totalIncome - totalExpense;
 
-    // show all value
+    // Set all value
     const showTotalExpense = document.getElementById('expense');
     showTotalExpense.innerText = totalExpense;
 
